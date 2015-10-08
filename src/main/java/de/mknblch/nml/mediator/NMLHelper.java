f949c9d4b0c69771227b44ba2d7ae92258ee58a2
Martin Knobloch <mknobloch@avantgarde-labs.de>
2015-10-08 13:02:00 +0200

    removed unused methods

--- src/main/java/de/mknblch/nml/mediator/NMLHelper.java
+++ src/main/java/de/mknblch/nml/mediator/NMLHelper.java
@@ -15,10 +15,6 @@ public class NMLHelper {
 
     public static final char[] UUID_CHARS = "abcdef0123456789".toCharArray();
 
-    public static String toKey(LOCATION location) {
-        return String.join("", location.getVOLUME(), location.getDIR(), location.getFILE());
-    }
-
     public static Path locationToPath(LOCATION location) {
         return Paths.get(locationToString(location.getVOLUME(), location.getDIR(), location.getFILE()));
     }
@@ -30,13 +26,6 @@ public class NMLHelper {
     public static Path primaryKeyToPath(String primaryKey) {
         primaryKey = primaryKey.replaceAll("/:", "/");
         return Paths.get(primaryKey);
-    }
-
-    public static PRIMARYKEY locationToPrimaryKey(LOCATION location) {
-        final PRIMARYKEY primarykey = new PRIMARYKEY();
-        primarykey.setTYPE("TRACK");
-        primarykey.setKEY(toKey(location));
-        return primarykey;
     }
 
     public static String locationToString(String... location) {
@@ -54,35 +43,6 @@ public class NMLHelper {
         return str.replaceAll("/", "/:");
     }
 
-    public static void addToPlaylistIfUnknown(PLAYLIST playlist, ENTRY entry) {
-        if (playlist.getENTRY().stream().anyMatch(e -> primaryKeyEquals(e, entry))) {
-            return;
-        }
-        playlist.getENTRY().add(entry);
-        normalizePlaylist(playlist);
-    }
-
-    public static void normalizeSubnodes(SUBNODES subnodes) {
-        subnodes.setCOUNT(subnodes.getNODE().size());
-    }
-
-    public static void normalizePlaylist(PLAYLIST playlist) {
-        playlist.setENTRIES(playlist.getENTRY().size());
-    }
-
-    public static boolean primaryKeyEquals(ENTRY a, ENTRY b) {
-        return getPrimaryKey(a).getKEY().equals(getPrimaryKey(b).getKEY());
-    }
-
-    public static PRIMARYKEY getPrimaryKey(ENTRY entry) {
-        final Object o = entry.getCONTENT().get(0);
-        if (o instanceof PRIMARYKEY) {
-            return (PRIMARYKEY) o;
-        }
-        throw new IllegalArgumentException("Invalid ENTRY Type");
-    }
-
-
     public static String generateUUID() {
         final Random random = new Random(System.currentTimeMillis());
         final StringBuilder builder = new StringBuilder();
@@ -90,19 +50,6 @@ public class NMLHelper {
             builder.append(UUID_CHARS[random.nextInt(UUID_CHARS.length)]);
         }
         return builder.toString();
-    }
-
-    public static ENTRY collectionEntryToPlaylistEntry(ENTRY entry) {
-
-        final Object e = entry.getCONTENT().get(0);
-
-        if (e instanceof PRIMARYKEY) {
-            throw new IllegalArgumentException("Invalid ENTRY Type - Origin must be COLLECTION");
-        }
-
-        final ENTRY plEntry = new ENTRY();
-        plEntry.getCONTENT().add(locationToPrimaryKey((LOCATION) e));
-        return plEntry;
     }
 
     public static <T> T getPrimaryContent(ENTRY e, Class<T> clazz) {
